ebd167b727eb8a14d8510cbae695203000d27471
mohannadghbashneh <ghbashneh99@gmail.com>
2023-01-10 17:14:20 +0200

    Hashtable01

--- python/code_challenges/hashtable/challenge01/challenge01.py
+++ python/code_challenges/hashtable/challenge01/challenge01.py
@@ -1,39 +1,24 @@
-# Write here the code challenge solution
-class TreeNode(object):
-    def __init__(self, value=0, left=None, right=None):
-        self.value = value
+
+
+class Tree:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
         self.left = left
         self.right = right
 
- # root = [7,2,9,1,5,null,14], k = 3   
-test = TreeNode(7)
-test.right = TreeNode(2)
-test.left = TreeNode(9)
-test.right.right = TreeNode(1)
-test.right.left = TreeNode(5)
-test.left.left = TreeNode(14)
 
+def find(root, k):
+   
+    arr = []
 
-
-print(test.value)
-
-def findTarget(Node,k):
-    '''
-     :type Node: TreeNode
-     :type k: int
-     :rtype: bool
-    '''
-    l = set()
-    def dfs(node):
+    def Two_Sum_BST(node):
         if not node:
             return False
-        y = k - node.value
-        if y in l:
+        x = k - node.val
+        if x in arr:
             return True
         else:
-            l.add(node.value)
-        return dfs(node.left) or dfs(node.right)
-    
-    return dfs(Node)
+            arr.append(node.val)
+        return Two_Sum_BST(node.left) or Two_Sum_BST(node.right)
 
-print(findTarget(test,15))
+    return Two_Sum_BST(root)
